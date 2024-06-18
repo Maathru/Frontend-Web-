745a0d86596c2c9308e757b6350227020d8933f3
vinupa <vinupathenuka@gmail.com>
2024-06-18 08:50:39 +0530

    navbar component updated

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,49 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import logo from "../assets/nav/maathru-nav-logo.png";
 import languageIcon from "../assets/nav/language-icon.png";
 import profilePhoto from "../assets/nav/sample-profile.png";
 import { MdLightMode } from "react-icons/md";
 import { MdNotifications } from "react-icons/md";
+import { MdMenu } from "react-icons/md";
+import { MdClose } from "react-icons/md";
+
+const navBarBg = "bg-[rgb(48,0,65,0.02)]";
 
 const navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const handleMenuClick = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
-    <div className="w-screen bg-slate-100 grid grid-cols-5">
-        <div className="col-span-1">
-            <img className="my-4 ml-8 w-20" src={logo} alt="" />
+    <div className="dark:bg-neutral-800">
+      <div className="flex justify-between items-center w-[92%] mx-auto">
+        <div className="">
+          <img className="md:w-16 w-12 p-1 my-1.5" src={logo} alt="" />
+        </div>
+        <div className={`${isMenuOpen ? "top-[10.5%] bg-white" : "top-[-100%]"} md:static absolute md:min-h-fit min-h-[50vh] left-0 md:w-auto w-full flex items-center px-5 duration-150`}>
+          <div className="flex md:flex-row flex-col md:items-center md:gap-[5vw] gap-8 text-lg font-medium md:ml-0 ml-4">
+            <Link>
+              <p className="hover:text-gray-500 text-black">Home</p>
+            </Link>
+            <Link>
+              <p className="hover:text-gray-500 text-[#9C33C1]">Blog</p>
+            </Link>
+            <Link>
+              <p className="hover:text-gray-500 text-black">Drugs</p>
+            </Link>
+            <Link>
+              <p className="hover:text-gray-500 text-black">Clinic</p>
+            </Link>
+            <Link>
+              <p className="hover:text-gray-500 text-black">Midwife</p>
+            </Link>
+            <Link>
+              <p className="hover:text-gray-500 text-black">Analysis</p>
+            </Link>
+          </div>
         </div>
 
-        <div className="nav-elements col-span-3 flex justify-around items-center text-xl font-medium">
-            <Link>
-                <p className="text-black">Home</p>
-            </Link>
-            <Link>
-                <p className="text-[#9C33C1]">Blog</p>
-            </Link>
-            <Link>
-                <p className="text-black">Drugs</p>
-            </Link>
-            <Link>
-                <p className="text-black">Clinic</p>
-            </Link>
-            <Link>
-                <p className="text-black">Midwife</p>
-            </Link>
-            <Link>
-                <p className="text-black">Analysis</p>
-            </Link>
+        <div className="nav-profile flex items-center justify-self-end gap-4 md:gap-6 text-black">
+          <div className="text-2xl hover:text-gray-500">
+            <MdLightMode />
+          </div>
+          <img className="w-8" src={languageIcon} alt="" />
+          <div className="text-2xl hover:text-gray-500">
+            <MdNotifications />
+          </div>
+          <img className="w-10" src={profilePhoto} alt="" />
+          <div className="text-3xl hover:text-gray-500 cursor-pointer md:hidden" onClick={handleMenuClick}>
+            {isMenuOpen ? <MdClose /> : <MdMenu />}
+          </div>
         </div>
-        <div className="nav-profile flex items-center justify-self-end text-black col-span-1">
-             <div className="text-4xl mx-2.5">
-                <MdLightMode />
-            </div>
-            <img className="mx-2.5 w-14" src={languageIcon} alt="" />
-            <div className="text-4xl mx-2.5">
-                <MdNotifications />
-            </div>
-            <img className="ml-3.5 mr-7 w-18" src={profilePhoto} alt="" />
-        </div>
-
+      </div>
     </div>
   );
 };
--- src/pages/blog.jsx
+++ src/pages/blog.jsx
@@ -2,19 +2,44 @@ import React from "react";
 import Navbar from "../components/navbar";
 import { MdOutlineArrowBackIosNew } from "react-icons/md";
 
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
 const blog = () => {
   return (
     <div className="w-screen h-screen bg-white">
-        <div>
-            <Navbar />
-            <div className="mt-10 ml-10 text-4xl font-semibold flex items-center text-black">
-                <MdOutlineArrowBackIosNew />
-                <p className="ml-3 text-5xl"> Blogs</p>
-            </div>
-            <p className="mt-16 ml-6 text-3xl font-semibold text-black">Recent Blogs</p>
-            
+      <div>
+        <Navbar />
+        <div className="md:mt-10 mt-5 md:ml-10 ml-3 text-3xl font-semibold flex items-center text-black">
+          <MdOutlineArrowBackIosNew />
+          <p className="ml-3 text-4xl"> Blogs</p>
         </div>
+        <p className="md:mt-12 mt-8 ml-6 text-3xl font-semibold text-black">
+          Recent Blogs
+        </p>
+      </div>
+
+      <Card>
+        <CardHeader>
+          <CardTitle>Card Title</CardTitle>
+          <CardDescription>Card Description</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p>Card Content</p>
+        </CardContent>
+        <CardFooter>
+          <p>Card Footer</p>
+        </CardFooter>
+      </Card>
+
     </div>
+    
   );
 };
 
