29acdd2584fcb60fe05036472adc219867cc212f
Buddhika Senanayake <bnsbuddhika@gmail.com>
2024-06-18 09:55:31 +0530

    Mode and nav color and custom fonts added

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,39 @@
-import { useState } from "react";
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useEffect, useState } from "react";
 import "./App.css";
 import Login from "./pages/login";
 import Signup from "./pages/signup";
 import Landing from "./pages/landing";
 import Drug from "./pages/drug";
+import { Button } from "flowbite-react";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [theme, setTheme] = useState(null);
+
+  useEffect(() => {
+    if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+      setTheme("dark");
+    } else {
+      setTheme("light");
+    }
+  }, []);
+
+  useEffect(() => {
+    if (theme === "dark") {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, [theme]);
+
+  const handleThemeSwitch = () => {
+    setTheme(theme === "dark" ? "light" : "dark");
+  };
 
   return (
-    <>
-      <Drug />
-    </>
+    <div className="bg-white dark:bg-neutral-800">
+      <Button onClick={handleThemeSwitch}>Mode Switch</Button>
+      <Login />
+    </div>
   );
 }
 
