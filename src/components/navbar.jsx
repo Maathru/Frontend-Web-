429e53bee422b53884e99415f0e8d04bd5c587db
vinupa <vinupathenuka@gmail.com>
2024-06-19 22:11:23 +0530

    blog page initial

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -8,8 +8,6 @@ import { MdNotifications } from "react-icons/md";
 import { MdMenu } from "react-icons/md";
 import { MdClose } from "react-icons/md";
 
-const navBarBg = "bg-[rgb(48,0,65,0.02)]";
-
 const navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
@@ -18,35 +16,35 @@ const navbar = () => {
   };
 
   return (
-    <div className="dark:bg-neutral-800">
+    <div className="bg-nav dark:bg-neutral-700">
       <div className="flex justify-between items-center w-[92%] mx-auto">
         <div className="">
           <img className="md:w-16 w-12 p-1 my-1.5" src={logo} alt="" />
         </div>
-        <div className={`${isMenuOpen ? "top-[10.5%] bg-white" : "top-[-100%]"} md:static absolute md:min-h-fit min-h-[50vh] left-0 md:w-auto w-full flex items-center px-5 duration-150`}>
+        <div className={`${isMenuOpen ? "top-[8%] bg-white dark:bg-neutral-700" : "top-[-100%]"} md:static absolute md:min-h-fit min-h-[50vh] left-0 md:w-auto w-full flex items-center px-5 duration-150`}>
           <div className="flex md:flex-row flex-col md:items-center md:gap-[5vw] gap-8 text-lg font-medium md:ml-0 ml-4">
             <Link>
-              <p className="hover:text-gray-500 text-black">Home</p>
+              <p className="hover:text-gray-500 text-black dark:text-gray-100">Home</p>
             </Link>
             <Link>
               <p className="hover:text-gray-500 text-[#9C33C1]">Blog</p>
             </Link>
             <Link>
-              <p className="hover:text-gray-500 text-black">Drugs</p>
+              <p className="hover:text-gray-500 text-black dark:text-gray-100">Drugs</p>
             </Link>
             <Link>
-              <p className="hover:text-gray-500 text-black">Clinic</p>
+              <p className="hover:text-gray-500 text-black dark:text-gray-100">Clinic</p>
             </Link>
             <Link>
-              <p className="hover:text-gray-500 text-black">Midwife</p>
+              <p className="hover:text-gray-500 text-black dark:text-gray-100">Midwife</p>
             </Link>
             <Link>
-              <p className="hover:text-gray-500 text-black">Analysis</p>
+              <p className="hover:text-gray-500 text-black dark:text-gray-100">Analysis</p>
             </Link>
           </div>
         </div>
 
-        <div className="nav-profile flex items-center justify-self-end gap-4 md:gap-6 text-black">
+        <div className="nav-profile flex items-center justify-self-end gap-4 md:gap-6 text-black dark:text-gray-100">
           <div className="text-2xl hover:text-gray-500">
             <MdLightMode />
           </div>
--- src/pages/blog.jsx
+++ src/pages/blog.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import Navbar from "../components/navbar";
+import Pagination from "../components/pagination";
+import Footer from "../components/footer";  
 import { MdOutlineArrowBackIosNew } from "react-icons/md";
+import BlogImage from "../assets/blog/blog-image.png";
 
 import {
   Card,
@@ -11,9 +14,11 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 
+import { Badge } from "@/components/ui/badge";
+
 const blog = () => {
   return (
-    <div className="w-screen h-screen bg-white">
+    <div className="bg-white">
       <div>
         <Navbar />
         <div className="md:mt-10 mt-5 md:ml-10 ml-3 text-3xl font-semibold flex items-center text-black">
@@ -21,25 +26,57 @@ const blog = () => {
           <p className="ml-3 text-4xl"> Blogs</p>
         </div>
         <p className="md:mt-12 mt-8 ml-6 text-3xl font-semibold text-black">
-          Recent Blogs
+          All Blogs
         </p>
       </div>
 
-      <Card>
-        <CardHeader>
-          <CardTitle>Card Title</CardTitle>
-          <CardDescription>Card Description</CardDescription>
-        </CardHeader>
-        <CardContent>
-          <p>Card Content</p>
-        </CardContent>
-        <CardFooter>
-          <p>Card Footer</p>
-        </CardFooter>
-      </Card>
+      <div className="grid grid-cols-1 md:grid-cols-2 md:gap-8 md:mt-8 mt-4 mx-4">
+        <Card className="bg-pink-50 flex flex-col justify-between">
+          <CardHeader>
+            <img src={BlogImage} alt="Blog Image" className="rounded-md mb-2" />
+            <CardTitle>Understanding Prenatal Nutrition</CardTitle>
+            <CardDescription>
+              What to Eat for a Healthy Pregnancy
+            </CardDescription>
+          </CardHeader>
+          <CardContent className="text-sm">
+            <p>
+              Maintaining a balanced diet during pregnancy is crucial for the
+              health of both the mother and the developing baby. In this post,
+              we'll explore essential nutrients needed during pregnancy, healthy
+              eating habits, and practical tips for managing common dietary
+              challenges. Discover how to create a nutritious meal plan that
+              supports a healthy pregnancy journey
+            </p>
+          </CardContent>
+          <CardFooter className="pb-0">
+            <div className="flex flex-wrap">
+              <Badge variant="secondary" className="bg-fuchsia-200 m-1">
+                Prenatal Nutrition
+              </Badge>
+              <Badge variant="secondary" className="bg-fuchsia-200 m-1">
+                Meal Plan
+              </Badge>
+              <Badge variant="secondary" className="bg-fuchsia-200 m-1">
+                Dietary Tips
+              </Badge>
+            </div>
+          </CardFooter>
+          <CardFooter className="text-sm flex justify-end text-[#9c3cc1]">
+            <p>Read More</p>
+          </CardFooter>
+        </Card>
+      </div>
+
+      <div>
+        <Pagination />
+      </div>
+      
+      <div>
+        <Footer />
+      </div>
 
     </div>
-    
   );
 };
 
