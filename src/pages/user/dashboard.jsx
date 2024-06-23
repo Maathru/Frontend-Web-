988e84a97de88c0b84658c02c9eb6aa7e106e59e
Buddhika Senanayake <bnsbuddhika@gmail.com>
2024-06-23 22:44:10 +0530

    Translations added

--- src/pages/user/dashboard.jsx
+++ src/pages/user/dashboard.jsx
@@ -14,6 +14,7 @@ import Select from "@mui/material/Select";
 import { useState } from "react";
 import { HiChatBubbleLeftRight } from "react-icons/hi2";
 import { LuPhoneCall } from "react-icons/lu";
+import { useTranslation } from "react-i18next";
 
 const provinces = [
   "Western",
@@ -34,30 +35,23 @@ const Dashboard = () => {
     setProvince(event.target.value);
   };
 
+  const { t } = useTranslation("userDashboard");
+
   return (
     <div className="container my-10 font-poppins">
       {/* Hero section */}
       <div className="flex">
         <div className="w-3/5 flex flex-col">
-          <h1 className="text-3xl">Welcome Buddhika</h1>
-          <p className="text-xl mt-8 ms-8 tracking-wider">
-            Explore our resources and connect with midwives
-          </p>
+          <h1 className="text-3xl">{t("greeting")} Buddhika</h1>
+          <p className="text-xl mt-8 ms-8 tracking-wider">{t("heading")}</p>
           <div className="w-10/12">
-            <p className="text-xl my-8 mx-14 text-center">
-              "Motherhood is a journey, and every step is worth and cherishing.”
-            </p>
+            <p className="text-xl my-8 mx-14 text-center">{t("title1")}</p>
             <p className="text-justify mx-14 tracking-wider">
-              At Maathru, we are dedicated to supporting you throughout your
-              journey to motherhood. Whether you're looking for expert advice,
-              personalized health support, or simply want to connect with a
-              community that understands, you'll find it all here. Our platform
-              is designed to provide you with the tools and information you need
-              to ensure a healthy and happy pregnancy.
+              {t("description1")}
             </p>
             <div className="flex items-center mt-12">
               <Button className="bg-primary-purple px-14 m-auto">
-                Learn More
+                {t("button1")}
               </Button>
             </div>
           </div>
@@ -74,25 +68,16 @@ const Dashboard = () => {
       {/* Service section */}
       <div className="mt-14">
         <div className="flex flex-col items-center px-24">
-          <h1 className="text-3xl font-medium">Our Services</h1>
+          <h1 className="text-3xl font-medium">{t("title2")}</h1>
           <p className="mt-12 tracking-wider text-center">
-            As a guest user, Maathru offers you a range of services to support
-            and guide you through your pregnancy journey.
-            <br />
-            From informative blog posts and vibrant community discussions to
-            essential vaccine cards, we provide the resources you need for a
-            healthy and informed pregnancy experience.
+            {t("description2")}
           </p>
           {/* Cards */}
           <div className="flex gap-10 mt-10 mb-10">
-            <WrapperCard title={"Visit Blog"} url={"/blogs"} image={blog} />
+            <WrapperCard title={t("card1Title")} url={"/blogs"} image={blog} />
+            <WrapperCard title={t("card2Title")} url={"/forum"} image={forum} />
             <WrapperCard
-              title={"Visit Discussion Forum"}
-              url={"/forum"}
-              image={forum}
-            />
-            <WrapperCard
-              title={"Get Vaccine Card"}
+              title={t("card3Title")}
               url={"/vaccine"}
               image={vaccine}
             />
@@ -107,19 +92,15 @@ const Dashboard = () => {
           role="alert"
         >
           <IoIosWarning size={35} color="white" />
-          <p className="text-white ps-3 pe-3">Status Updated</p>
+          <p className="text-white ps-3 pe-3">{t("alert")}</p>
           <IoMdCloseCircle className="absolute -top-2 -right-2" size={16} />
         </div>
 
         <div className="flex flex-col items-center">
-          <h1 className="text-3xl font-medium mt-16 px-8">
-            You are now eligible!
-          </h1>
-          <p className="text-xl m-4">
-            Please fill out the necessary details to complete the form.
-          </p>
+          <h1 className="text-3xl font-medium mt-16 px-8">{t("title3")}</h1>
+          <p className="text-xl m-4">{t("description3")}</p>
           <Button className="bg-primary-purple mt-4 mb-10">
-            Go to the Form
+            {t("button2")}
           </Button>
         </div>
       </div>
@@ -127,11 +108,10 @@ const Dashboard = () => {
       {/* Contact */}
       <div className="mt-14 mx-40">
         <div className="flex flex-col items-center">
-          <h1 className="text-3xl font-medium">Contact Your Midwife</h1>
+          <h1 className="text-3xl font-medium">{t("title4")}</h1>
         </div>
-        <h2 className="text-2xl font-medium mt-8">
-          Select the area to contact:
-        </h2>
+        <h2 className="text-2xl font-medium mt-8">{t("description4")}</h2>
+
         {/* Dropdown container */}
         <div className="flex flex-col items-center">
           <div className="flex items-center mt-8 gap-14">
@@ -139,13 +119,13 @@ const Dashboard = () => {
             <Box sx={{ minWidth: 240, maxWidth: 300 }}>
               <FormControl fullWidth>
                 <InputLabel id="province-select-label">
-                  Select the Province
+                  {t("dropdown1")}
                 </InputLabel>
                 <Select
                   labelId="province-select-label"
                   id="province-select"
                   value={province}
-                  label="Select the Province"
+                  label={t("dropdown1")}
                   onChange={handleChange}
                 >
                   {provinces.map((province) => (
@@ -160,13 +140,13 @@ const Dashboard = () => {
             <Box sx={{ minWidth: 240, maxWidth: 300 }}>
               <FormControl fullWidth>
                 <InputLabel id="district-select-label">
-                  Select the Area
+                  {t("dropdown2")}
                 </InputLabel>
                 <Select
                   labelId="district-select-label"
                   id="district-select"
                   value={province}
-                  label="Select the Area"
+                  label={t("dropdown2")}
                   onChange={handleChange}
                 >
                   {provinces.map((province) => (
@@ -180,14 +160,12 @@ const Dashboard = () => {
             {/* Area */}
             <Box sx={{ minWidth: 240, maxWidth: 300 }}>
               <FormControl fullWidth>
-                <InputLabel id="area-select-label">
-                  Select the District
-                </InputLabel>
+                <InputLabel id="area-select-label">{t("dropdown3")}</InputLabel>
                 <Select
                   labelId="area-select-label"
                   id="area-select"
                   value={province}
-                  label="Select the District"
+                  label={t("dropdown3")}
                   onChange={handleChange}
                 >
                   {provinces.map((province) => (
@@ -204,36 +182,34 @@ const Dashboard = () => {
         <div className="c rounded-xl m-8 shadow-md">
           <div className="flex">
             <div className="w-1/2 m-8">
-              <h2 className="text-xl font-medium">
-                Midwife details of your area:
-              </h2>
+              <h2 className="text-xl font-medium">{t("midwife")}</h2>
               <div className="mx-8 mt-5">
                 <div className="flex my-3 gap-2">
-                  <p>Name :</p>
+                  <p>{t("name")}</p>
                   <p>Buddhika Senanayake</p>
                 </div>
                 <div className="flex my-3 gap-2">
-                  <p>Email :</p>
+                  <p>{t("email")}</p>
                   <p>bnsbuddhika@gmail.com</p>
                 </div>
                 <div className="flex my-3 gap-2">
-                  <p>Telephone Number :</p>
+                  <p>{t("phone")}</p>
                   <p>071 327 0510</p>
                 </div>
               </div>
             </div>
             <div className="w-1/2 m-8">
-              <h2 className="text-xl font-medium">Contact now:</h2>
+              <h2 className="text-xl font-medium">{t("contact")}</h2>
 
               <Button className="bg-primary-purple mx-8 mt-5">
                 <HiChatBubbleLeftRight size={25} color="white" />
-                <p className="text-white ps-2 pe-3">Message</p>
+                <p className="text-white ps-2 pe-3">{t("message")}</p>
               </Button>
               <br />
 
               <Button className="bg-primary-purple mx-8 mt-4">
                 <LuPhoneCall size={25} color="white" />
-                <p className="text-white ps-2 pe-3">Call Now</p>
+                <p className="text-white ps-2 pe-3">{t("call")}</p>
               </Button>
             </div>
           </div>
