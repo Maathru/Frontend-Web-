704181b7b1448d9e78cd03aed693ab4ed301bac7
Buddhika Senanayake <bnsbuddhika@gmail.com>
2024-12-03 01:45:47 +0530

    Backend connection error fixed

--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -1,47 +1,43 @@
-import React, { useState, useEffect, useContext } from "react";
+import React, { useEffect, useState } from "react";
 import { TextField, Avatar, Box, Typography } from "@mui/material";
 import { styled } from "@mui/system";
 import { Button } from "@/components/ui/button";
 import UserService from "@/service/userService";
-import { userData } from "@/context/userAuth";
+import { errorType, Toast } from "@/components/toast";
+import { format } from "date-fns";
+import { role } from "@/data/roleData";
 
 const Input = styled("input")({
   display: "none",
 });
 
+const Capitalize = (str) => {
+  if (!str) return str;
+
+  return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+};
+
+const FormattedDateTime = (params) => {
+  if (!params) return "";
+
+  const date = new Date(params);
+
+  const readableDate = format(date, "MMMM do, yyyy"); // "July 26th, 2024"
+  const readableTime = format(date, "hh:mm:ss a"); // "06:39:04 PM"
+
+  return `${readableTime} at ${readableDate}`;
+};
+
 const Profile = () => {
-  const [formData, setFormData] = useState({
-    firstName: "",
-    lastName: "",
-    email: "",
-    password: "",
-    profilePicture: null,
-  });
-
-  const { userDetails } = useContext(userData); 
-
-  useEffect(() => {
-    const fetchProfile = async () => {
-      try {
-        const email = userDetails?.userId; 
-        if (email) {
-          const profileData = await UserService.getUserIdByEmail(email);
-          setFormData(profileData);
-        } else {
-          console.error("No email found in user details");
-        }
-      } catch (error) {
-        console.error("Error fetching profile data:", error);
-      }
-    };
-
-    fetchProfile();
-  }, [userDetails]);
-  
+  const [formData, setFormData] = useState({});
+  const [errors, setErrors] = useState({});
+  const [isUpdated, setIsUpdated] = useState(false);
+  const [editMode, setEditMode] = useState(false);
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
-    setFormData((prevData) => ({ ...prevData, [name]: value }));
+    setFormData({ ...formData, [name]: value });
+    setErrors({ ...errors, [name]: validateField(name, value) });
   };
 
   const handleProfilePictureChange = (e) => {
@@ -49,7 +45,10 @@ const Profile = () => {
     if (file && file.type.startsWith("image/")) {
       const reader = new FileReader();
       reader.onload = () => {
-        setFormData((prevData) => ({ ...prevData, profilePicture: reader.result }));
+        setFormData((prevData) => ({
+          ...prevData,
+          profilePicture: reader.result,
+        }));
       };
       reader.readAsDataURL(file);
     } else {
@@ -57,10 +56,137 @@ const Profile = () => {
     }
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log("Profile Data Submitted:", formData);
-    // You can also send formData to an API here to update the profile
+    if (!editMode) return;
+
+    const validationErrors = validate();
+
+    if (Object.keys(validationErrors).length !== 0) {
+      setErrors(validationErrors);
+      return;
+    }
+
+    try {
+      const response = await UserService.updateProfile(formData);
+
+      setFormData({
+        firstName: "",
+        lastName: "",
+        email: "",
+        role: "",
+        phoneNumber: "",
+        nic: "",
+        dob: "",
+        gender: "",
+        addressLine1: "",
+        street: "",
+        city: "",
+        designation: "",
+        qualifications: "",
+        lastLogin: "",
+      });
+      Toast(response, errorType.SUCCESS);
+
+      setIsUpdated(true);
+      setEditMode(false);
+    } catch (error) {
+      console.log(error.message);
+
+      const data = error.response.data;
+      if (data) {
+        if (Array.isArray(data)) {
+          const newErrors = {};
+          data.map((msg) => {
+            Toast(msg.message, errorType.ERROR);
+            newErrors[msg.field] = msg.message;
+          });
+
+          setErrors(newErrors);
+        } else {
+          console.log(data);
+          Toast(data || "Error occurred", errorType.ERROR);
+        }
+      }
+    }
+  };
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const response = await UserService.getUser();
+        setFormData(response);
+      } catch (error) {
+        console.log(error.message);
+
+        const data = error.response.data;
+        console.log(data);
+        Toast(data || "Error occurred", errorType.ERROR);
+      }
+    };
+
+    fetchUser();
+  }, [isUpdated]);
+
+  const validateField = (name, value) => {
+    if (
+      formData.role == role.ADMIN ||
+      formData.role == role.DOCTOR ||
+      formData.role == role.MIDWIFE
+    ) {
+      switch (name) {
+        case "firstName":
+          if (!value) return "First name is required";
+          break;
+        case "lastName":
+          if (!value) return "Last name is required";
+          break;
+        case "phoneNumber":
+          if (!value) return "Phone number is required";
+          break;
+        case "nic":
+          if (!value) return "NIC number is required";
+          break;
+        case "addressLine1":
+          if (!value) return "Address line 1 is required";
+          break;
+        case "street":
+          if (!value) return "Street is required";
+          break;
+        case "city":
+          if (!value) return "City is required";
+          break;
+        case "designation":
+          if (!value) return "Designation is required";
+          break;
+        default:
+          break;
+      }
+      return "";
+    } else {
+      switch (name) {
+        case "firstName":
+          if (!value) return "First name is required";
+          break;
+        case "lastName":
+          if (!value) return "Last name is required";
+          break;
+        default:
+          break;
+      }
+      return "";
+    }
+  };
+
+  const validate = () => {
+    const newErrors = {};
+
+    Object.keys(formData).forEach((key) => {
+      const error = validateField(key, formData[key]);
+      if (error) newErrors[key] = error;
+    });
+
+    return newErrors;
   };
 
   return (
@@ -79,8 +205,10 @@ const Profile = () => {
         boxShadow: 2,
       }}
     >
+      {!editMode && <Button onClick={() => setEditMode(true)}>Edit</Button>}
+
       <Typography variant="h5" sx={{ mb: 3 }}>
-        Edit Profile
+        {editMode ? "Edit Profile" : "View Profile"}
       </Typography>
       <Avatar
         src={formData.profilePicture}
@@ -98,55 +226,205 @@ const Profile = () => {
           {formData.profilePicture ? "Change Picture" : "Upload Picture"}
         </Button>
       </label>
+
       <form onSubmit={handleSubmit} style={{ width: "100%", marginTop: 16 }}>
         <TextField
+          disabled={!editMode}
           fullWidth
           label="First Name"
           name="firstName"
-          value={formData.firstName}
+          value={formData.firstName || ""}
           onChange={handleInputChange}
           margin="normal"
           variant="outlined"
         />
         <TextField
+          disabled={!editMode}
           fullWidth
           label="Last Name"
           name="lastName"
-          value={formData.lastName}
+          value={formData.lastName || ""}
           onChange={handleInputChange}
           margin="normal"
           variant="outlined"
         />
-        <TextField
-          fullWidth
-          label="Email"
-          name="email"
-          value={formData.email}
-          onChange={handleInputChange}
-          margin="normal"
-          type="email"
-          variant="outlined"
-        />
-        <TextField
-          fullWidth
-          label="Password"
-          name="password"
-          value={formData.password}
-          onChange={handleInputChange}
-          margin="normal"
-          type="password"
-          variant="outlined"
-        />
-        <Box sx={{ display: "flex", justifyContent: "center", width: "100%" }}>
-          <Button
-            type="submit"
-            variant="default"
-            color="primary"
-            sx={{ mt: 2, width: "100%", maxWidth: "500px" }}
+
+        {!editMode && (
+          <TextField
+            disabled
+            fullWidth
+            label="Email"
+            value={formData.email || ""}
+            margin="normal"
+            type="email"
+            variant="outlined"
+          />
+        )}
+
+        {(formData.role == role.ADMIN ||
+          formData.role == role.DOCTOR ||
+          formData.role == role.MIDWIFE) && (
+          <>
+            <TextField
+              disabled={!editMode}
+              fullWidth
+              label="Phone Number"
+              name="phoneNumber"
+              value={formData.phoneNumber || ""}
+              onChange={handleInputChange}
+              margin="normal"
+              variant="outlined"
+            />
+
+            <TextField
+              disabled={!editMode}
+              fullWidth
+              label="NIC"
+              name="nic"
+              value={formData.nic || ""}
+              onChange={handleInputChange}
+              margin="normal"
+              variant="outlined"
+            />
+
+            {!editMode && (
+              <>
+                <TextField
+                  disabled
+                  fullWidth
+                  label="Gender"
+                  value={Capitalize(formData.gender) || ""}
+                  margin="normal"
+                  variant="outlined"
+                />
+
+                <TextField
+                  disabled
+                  fullWidth
+                  label="Date of Birth"
+                  value={formData.dob || ""}
+                  margin="normal"
+                  variant="outlined"
+                />
+              </>
+            )}
+
+            <TextField
+              disabled={!editMode}
+              fullWidth
+              label="Address Line 1"
+              name="addressLine1"
+              value={formData.addressLine1 || ""}
+              onChange={handleInputChange}
+              margin="normal"
+              variant="outlined"
+            />
+
+            <TextField
+              disabled={!editMode}
+              fullWidth
+              label="Street"
+              name="street"
+              value={formData.street || ""}
+              onChange={handleInputChange}
+              margin="normal"
+              variant="outlined"
+            />
+
+            <TextField
+              disabled={!editMode}
+              fullWidth
+              label="City"
+              name="city"
+              value={formData.city || ""}
+              onChange={handleInputChange}
+              margin="normal"
+              variant="outlined"
+            />
+
+            <TextField
+              disabled={!editMode}
+              fullWidth
+              label="Designation"
+              name="designation"
+              value={formData.designation || ""}
+              onChange={handleInputChange}
+              margin="normal"
+              variant="outlined"
+            />
+
+            <TextField
+              disabled={!editMode}
+              fullWidth
+              label="Qualifications"
+              name="qualifications"
+              value={formData.qualifications || ""}
+              onChange={handleInputChange}
+              margin="normal"
+              variant="outlined"
+            />
+
+            {!editMode && (
+              <>
+                <TextField
+                  disabled
+                  fullWidth
+                  label="MOH"
+                  value={Capitalize(formData.moh) || ""}
+                  margin="normal"
+                  variant="outlined"
+                />
+
+                <TextField
+                  disabled
+                  fullWidth
+                  label="Region"
+                  value={Capitalize(formData.region) || ""}
+                  margin="normal"
+                  variant="outlined"
+                />
+              </>
+            )}
+          </>
+        )}
+
+        {!editMode && (
+          <>
+            <TextField
+              disabled
+              fullWidth
+              label="Role"
+              value={Capitalize(formData.role) || ""}
+              margin="normal"
+              variant="outlined"
+            />
+
+            <TextField
+              disabled
+              fullWidth
+              label="Last login"
+              value={FormattedDateTime(formData.lastLogin) || ""}
+              margin="normal"
+              variant="outlined"
+            />
+          </>
+        )}
+
+        {editMode && (
+          <Box
+            sx={{ display: "flex", justifyContent: "center", width: "100%" }}
           >
-            Save Changes
-          </Button>
-        </Box>
+            <Button
+              disabled={!editMode}
+              type="submit"
+              variant="default"
+              color="primary"
+              sx={{ mt: 2, width: "100%", maxWidth: "500px" }}
+            >
+              Save Changes
+            </Button>
+          </Box>
+        )}
       </form>
     </Box>
   );
