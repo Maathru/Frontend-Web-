c06100111e2c5e166b55706bda79c627a9e3da30
Buddhika Senanayake <bnsbuddhika@gmail.com>
2024-12-03 23:34:49 +0530

    Home visits for midwife

---
+++ src/service/visitsService.js
@@ -0,0 +1,43 @@
+import axiosInstance from "./axiosInstance";
+
+class VisitsService {
+  static async saveHomeVisits(formData) {
+    try {
+      const response = await axiosInstance.post(`/visits`, {
+        ...formData,
+      });
+      return response.data;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  static async getHomeVisits(userId) {
+    try {
+      const response = await axiosInstance.get(`/visits/${userId}`);
+      return response.data;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  static async getHomeVisitsByMonthForMidwife(date) {
+    try {
+      const response = await axiosInstance.get(`/visits/month/midwife/${date}`);
+      return response.data;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  static async getHomeVisitsByDateForMidwife(date) {
+    try {
+      const response = await axiosInstance.get(`/visits/date/midwife/${date}`);
+      return response.data;
+    } catch (error) {
+      throw error;
+    }
+  }
+}
+
+export default VisitsService;
